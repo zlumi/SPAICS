0d07366722a8945e0834c4e6a98cbd2c65b6de1e
zClapr <48451623+zLuminate@users.noreply.github.com>
2023-03-31 09:07:23 +0200

    launch!

--- app/datastream/page.js
+++ app/datastream/page.js
@@ -1,14 +1,12 @@
 'use client'
 
-import Script from 'next/script';
 import './datas.css';
+import { useEffect } from 'react';
 // import AAER_Model from "../components/cansat_aaer"
 // import Inertia_Model from "../components/cansat_inertia"
 
 export default function DataStreamPage() {
-    return (
-        <Script id="L">{
-            window.location.href = "/datastream/aaer"
-        }</Script>
-    )
+    useEffect(() => {
+        window.location.href = "/datastream/aaer"
+    }, [])
 }
