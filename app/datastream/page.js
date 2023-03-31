2a80c3e3d115cc1313491bfb980941f7bedf4314
zClapr <48451623+zLuminate@users.noreply.github.com>
2023-03-31 08:54:59 +0200

    launchhhh!!!

--- app/datastream/page.js
+++ app/datastream/page.js
@@ -1,13 +1,11 @@
 'use client'
 
 import './datas.css';
-import { Redirect } from 'react-router-dom';
 // import AAER_Model from "../components/cansat_aaer"
 // import Inertia_Model from "../components/cansat_inertia"
 
 export default function DataStreamPage() {
     return (
-        // redirect to /aaer automatically on load
-        <Redirect to="/aaer" />
+        location.href = "/datastream/aaer"
     )
 }
