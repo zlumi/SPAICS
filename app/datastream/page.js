1a18aa1b9bcb54bf7e03506dcb843e8f990bcf6a
zClapr <48451623+zLuminate@users.noreply.github.com>
2023-03-31 08:59:16 +0200

    launch!!!

--- app/datastream/page.js
+++ app/datastream/page.js
@@ -1,11 +1,12 @@
 'use client'
 
+import Script from 'next/script';
 import './datas.css';
 // import AAER_Model from "../components/cansat_aaer"
 // import Inertia_Model from "../components/cansat_inertia"
 
 export default function DataStreamPage() {
     return (
-        location.href = "/datastream/aaer"
+        window.location.href = "/datastream/aaer"
     )
 }
